00ebc8726154b367e952835fa131e03c4935f7ab
Angel Valenzuela <avalenzuelab3@miumg.edu.gt>
2026-04-24 14:11:59 -0600

    Ejercicio1

---
+++ src/polimorfismo/Ejercicio1.java
@@ -0,0 +1,33 @@
+package polimorfismo;
+
+
+
+
+class Animal {
+
+ public void hacerSonido() {
+     System.out.println("El animal hace un sonido");
+ }
+}
+
+
+class Perro extends Animal {
+
+ @Override
+ public void hacerSonido() {
+     System.out.println("El perro ladra: Guau Guau");
+ }
+}
+
+
+public class Ejercicio1 {
+
+ public static void main(String[] args) {
+
+    
+     Animal miAnimal = new Perro();
+
+    
+     miAnimal.hacerSonido();
+ }
+}
